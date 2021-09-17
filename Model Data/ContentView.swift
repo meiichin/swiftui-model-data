14e74be2c07ff905599545f44be41737f8f92bd8
meiichin <meiichan3017@gmail.com>
2021-09-17 20:44:47 +0700

    Initial Commit

---
+++ Model Data/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Model Data
+//
+//  Created by Yuu on 17/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Model Data/Model_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Model_DataApp.swift
+//  Model Data
+//
+//  Created by Yuu on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Model_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
