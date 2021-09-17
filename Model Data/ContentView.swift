9b71381fd56eecd1367c7cb16b6a0bc6951528a8
meiichin <meiichan3017@gmail.com>
2021-09-17 21:33:17 +0700

    [Added] model product

--- Model Data/ContentView.swift
+++ Model Data/ContentView.swift
@@ -19,6 +19,28 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// Model product
+struct ProductModel : Identifiable {
+    let id: Int
+    let productName: String
+    let productImage: String
+    let productPrice: Int
+    let location: String
+    let rateCount: Int
+    let rateTotal: Int
+    
+    init(id: Int, productname: String, productimage: String, productprice: Int, location: String, ratecount: Int, ratetotal: Int) {
+        
+        self.id = id
+        self.productName = productname
+        self.productImage = productimage
+        self.productPrice = productprice
+        self.location = location
+        self.rateCount = ratecount
+        self.rateTotal = ratetotal
+    }
+}
+
 // Component product
 struct ProductView: View {
     var body: some View{
