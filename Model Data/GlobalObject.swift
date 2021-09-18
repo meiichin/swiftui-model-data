cde9c5192984b44bd595d002e160236820be83bf
meiichin <meiichan3017@gmail.com>
2021-09-18 14:22:38 +0700

    [Added] class observeable

---
+++ Model Data/GlobalObject.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalObject.swift
+//  Model Data
+//
+//  Created by Yuu on 18/09/21.
+//
+
+import Foundation
+
+class GlobalObject: ObservableObject {
+    @Published var total : Int = 0
+}
